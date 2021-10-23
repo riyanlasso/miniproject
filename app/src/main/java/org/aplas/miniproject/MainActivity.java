44e9ac2765857f9cde3881d1a76afb69ffab1b44
riyanlasso <riyanlasso@gmail.com>
2021-10-23 22:34:58 +0700

    tambah button kembali

--- app/src/main/java/org/aplas/miniproject/MainActivity.java
+++ app/src/main/java/org/aplas/miniproject/MainActivity.java
@@ -8,14 +8,14 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn_sign_in;
+    private Button btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn_sign_in = (Button) findViewById(R.id.btn_sign_in);
-        btn_sign_in.setOnClickListener(new View.OnClickListener() {
+        btn_register = (Button) findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openActivity_register();
