fcba553243bcbe4ac368044466b8de9707133865
riyanlasso <riyanlasso@gmail.com>
2021-10-23 10:18:13 +0700

    halaman register , add strings , java , dsb

--- app/src/main/java/org/aplas/miniproject/MainActivity.java
+++ app/src/main/java/org/aplas/miniproject/MainActivity.java
@@ -1,13 +1,30 @@
 package org.aplas.miniproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn_sign_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_sign_in = (Button) findViewById(R.id.btn_sign_in);
+        btn_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity_register();
+            }
+        });
+    }
+
+    public void openActivity_register() {
+        Intent intent = new Intent(this,activity_register.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/org/aplas/miniproject/activity_register.java
@@ -0,0 +1,14 @@
+package org.aplas.miniproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
