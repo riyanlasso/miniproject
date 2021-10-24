b749e6c83745946b76ed63e0fdb758f8b17dcd50
riyanlasso <riyanlasso@gmail.com>
2021-10-24 19:32:47 +0700

    add halaman utama , button belum berfungsi

---
+++ app/src/main/java/org/aplas/miniproject/pesan_tiket.java
@@ -0,0 +1,30 @@
+package org.aplas.miniproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class pesan_tiket extends AppCompatActivity {
+    private Button btn_back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pesan_tiket);
+        btn_back = (Button) findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openlogin();
+            }
+        });
+    }
+
+    public void openlogin() {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/org/aplas/miniproject/pilihkursi.java
@@ -0,0 +1,14 @@
+package org.aplas.miniproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pilihkursi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pilihkursi);
+    }
+}
