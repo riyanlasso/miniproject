ea02187e04477f51415ec3b917c1f38a81236222
riyanlasso <riyanlasso@gmail.com>
2021-10-25 08:05:48 +0700

    add logout button

--- app/src/main/java/org/aplas/miniproject/home.java
+++ app/src/main/java/org/aplas/miniproject/home.java
@@ -8,17 +8,24 @@ import android.view.View;
 import android.widget.Button;
 
 public class home extends AppCompatActivity {
-    private Button btn_pesan;
+    private Button btn_pesan , btn_logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         btn_pesan = (Button) findViewById(R.id.btn_pesan);
+        btn_logout = (Button) findViewById(R.id.btn_logout);
         btn_pesan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openTicket();
+            }
+        });
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout();
             }
         });
 
@@ -27,5 +34,9 @@ public class home extends AppCompatActivity {
     public void openTicket() {
         Intent intent = new Intent(this,pilihkursi.class);
         startActivity(intent);
+    }
+    public void logout() {
+        Intent intent1 = new Intent(this,MainActivity.class);
+        startActivity(intent1);
     }
 }
